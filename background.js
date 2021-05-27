6344551d8360ceca30124de733ae9bfe59bebd50
Abhijeeth S <abhikampurath@gmail.com>
2021-05-27 14:41:59 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,20 +1,5 @@
-//let count = 0
-// chrome.tabs.query({ windowType: "normal" }, function (tabs) {
-//      count = tabs.length;
-//      console.log("count = " + count);
-//      chrome.storage.local.set({ tabCount: count }, function () {
-//          console.log("value is set to" + count);
-//      });
-//  });
-
-// chrome.browserAction.onClicked.addListener((tabs) => {
-//     chrome.tabs.executeScript(null, { file: "background.js" })
-// });
-
 chrome.tabs.onCreated.addListener(function (){
     chrome.tabs.query({ windowType: "normal" }, function (tabs) {
-        //count = tabs.length;
-        //console.log("count = " + count);
         chrome.storage.local.set({ tabCount: tabs.length }, function () {
             console.log("value is set to" + tabs.length);
         });
@@ -23,8 +8,6 @@ chrome.tabs.onCreated.addListener(function (){
 
 chrome.tabs.onRemoved.addListener(function (){
     chrome.tabs.query({ windowType: "normal" }, function (tabs) {
-    //count = tabs.length;
-    //console.log("count = " + count);
     chrome.storage.local.set({ tabCount: tabs.length }, function () {
         console.log("value is set to" + tabs.length);
     });
